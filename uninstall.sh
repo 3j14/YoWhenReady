ebab9542d31316cb186e9e70f822a13acfbaac12
Jonas Drotleff <jonas.drotleff@gmail.com>
2016-02-10 23:48:37 +0100

    uninstall yo

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,14 @@
 #/bin/bash
 
 DIRINSTALL=~/.yo
-
-rm -r $DIRINSTALL
-echo "All done. Bye!"
+while true; do
+    read -p "You are about to uninstall yo. Continue? Y/n " yn
+    case $yn in
+        [Yy]* )
+        rm -r $DIRINSTALL
+        echo "All done. Bye!"
+        break;;
+        * ) echo "Uninstallation aborted!"
+        break;;
+    esac
+done
